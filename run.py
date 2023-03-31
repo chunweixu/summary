12717d43b96d943c1c3e9af3f7b7aecc040925e8
smiling <smiling@smilingdeMacBook-Air.local>
2023-03-31 14:29:39 +0800

    update

--- run.py
+++ run.py
@@ -10,21 +10,23 @@ model_name = "../autodl-tmp/pertrained_model/chatglm-6B"
 tokenizer = AutoTokenizer.from_pretrained(model_name, trust_remote_code=True)
 model = AutoModel.from_pretrained(model_name, trust_remote_code=True).half().cuda()
 
-train_data = {"text": read_file("data/dataset/LCSTS/train.src.txt")[:10], "target": read_file("data/dataset/LCSTS/train.tgt.txt")[:10]}
-test_data = {"text": read_file("data/dataset/LCSTS/test.src.txt")[:1000], "target": read_file("data/dataset/LCSTS/test.tgt.txt")[:1000]}
-# begin = "我给你一些文本标题生成的示例模版供你学习，然后根据学习的内容进行文本标题生成"
-# response, history = model.chat(tokenizer, begin, history=[])
-# prompt_train = "文本："
-# prompt_train_label = "标题："
-# for t, l in zip(train_data["text"], train_data["target"]):
-#     response, history = model.chat(tokenizer, prompt_train+t+' '+prompt_train_label+l, history=[])
-#     print(response)
-print("generactor text summary.")
+train_data = {"text": read_file("data/dataset/LCSTS/train.src.txt")[:20], "target": read_file("data/dataset/LCSTS/train.tgt.txt")[:20]}
+test_data = {"text": read_file("data/dataset/LCSTS/test.src.txt")[:100], "target": read_file("data/dataset/LCSTS/test.tgt.txt")[:100]}
+begin = "请生成文本的标题"
+response, history = model.chat(tokenizer, begin, history=[])
+prompt_train = "文本："
+prompt_train_label = "标题："
+for t, l in zip(train_data["text"], train_data["target"]):
+    # response, history = model.chat(tokenizer, prompt_train+t+' '+prompt_train_label+l, history=[])
+    response, history = model.chat(tokenizer, t + " => " + l, history=[])
+    print(response)
+print("generactor text title.")
 prompt = "请给下面一段文本生成标题："
 predict = []
 for t in test_data['text']:
     # response, history = model.chat(tokenizer, prompt_train+t+' '+prompt_train_label, history=[])
-    response, history = model.chat(tokenizer, prompt+t, history=[])
+    # response, history = model.chat(tokenizer, prompt+t, history=[])
+    response, history = model.chat(tokenizer, prompt + " => ", history=[])
     predict.append(' '.join(list(response.strip())))
     print(response)
 target = [' '.join(list(tar)) for tar in test_data['target']]
