bea64b1fbcc0d05cd31eb5d1fb6807e2f30fc55a
smiling <smiling@smilingdeMacBook-Air.local>
2023-03-31 11:56:23 +0800

    update

--- run.py
+++ run.py
@@ -10,13 +10,13 @@ model_name = "../autodl-tmp/pertrained_model/chatglm-6B"
 tokenizer = AutoTokenizer.from_pretrained(model_name, trust_remote_code=True)
 model = AutoModel.from_pretrained(model_name, trust_remote_code=True).half().cuda()
 
-test_data = {"text": read_file("data/dataset/LCSTS/test.src.txt")[:1000], "target": read_file("data/dataset/LCSTS/test.tgt.txt")[:1000]}
+test_data = {"text": read_file("data/dataset/LCSTS/test.src.txt")[:10], "target": read_file("data/dataset/LCSTS/test.tgt.txt")[:10]}
 prompt = "请给下面一段文本做摘要："
 predict = []
 for t in test_data['text']:
     response, history = model.chat(tokenizer, prompt+t, history=[])
     predict.append(' '.join(list(response.strip())))
     print(response)
-target = [' '.join(list(tar) for tar in test_data['target'])]
+target = [' '.join(list(tar)) for tar in test_data['target']]
 avg_rouge_scores = rouge.get_scores(predict, target, avg=True)
 print(avg_rouge_scores)
